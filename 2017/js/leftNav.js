0b37f81f5b8dc2641d2ce7c3d861ab857e9aef52
HEWQ <wenqiu.he@yahoo.com>
2016-12-19 18:27:45 +0800

    2017

--- 2017/js/leftNav.js
+++ 2017/js/leftNav.js
@@ -5,9 +5,17 @@
 $(function(){
     /*leftNav*/
     $(".left-nav-li").click(function(){
-        $(".left-nav-li").removeClass("active");
+            $(".left-nav-li").removeClass("active");
+            $(this).addClass("active");
+        });
+
+    /*topNav*/
+    $(".top-nav-li span").click(function(){
+        $(".top-nav-li span").removeClass("active");
         $(this).addClass("active");
     });
+
     /*table css*/
     $(".main-tb tr:even").not("main-th").css("background-color","#f8f9fb");
+
 });
