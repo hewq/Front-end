4e019f642e39eec7cc823ce70a1e59e1512c7240
wenqiu.he <wenqiu.he@yahoo.com>
2018-05-02 19:22:18 +0800

    管理资源

--- webpack/webpack-demo/src/index.js
+++ webpack/webpack-demo/src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './share-icon.png';
+import Data from './data.xml';
 
 function component() {
 	var element = document.createElement('div');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
 
+	// 将图像添加到现有的div
+	var myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
+
+	console.log(Data);
+	
 	return element;
 }
 document.body.appendChild(component());
