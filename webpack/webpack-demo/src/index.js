fa0cdf291fd4434b53c78512c80c2b345c8f7790
wenqiu.he <wenqiu.he@yahoo.com>
2018-05-03 14:23:10 +0800

    管理输出

--- webpack/webpack-demo/src/index.js
+++ webpack/webpack-demo/src/index.js
@@ -2,9 +2,11 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './share-icon.png';
 import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
 	var element = document.createElement('div');
+	var btn = document.createElement('button');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 	element.classList.add('hello');
@@ -16,6 +18,11 @@ function component() {
 	element.appendChild(myIcon);
 
 	console.log(Data);
+
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+
+	element.appendChild(btn);
 	
 	return element;
 }
--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -1,10 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js'
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title: 'Output Management'
+		})
+	],
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')	//“__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录
 	},
 	module: {
 		rules: [
