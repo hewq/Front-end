033f01997a9f3c6275844ac587d7f6731b604ae9
wenqiu.he <wenqiu.he@yahoo.com>
2018-05-04 14:46:03 +0800

    生产环境构建

--- webpack/webpack-demo/src/index.js
+++ webpack/webpack-demo/src/index.js
@@ -1,9 +1,13 @@
 // import _ from 'lodash';
 import { cube } from './math.js';
-import './style.css';
-import Icon from './share-icon.png';
-import Data from './data.xml';
+// import './style.css';
+// import Icon from './share-icon.png';
+// import Data from './data.xml';
 import printMe from './print.js';
+
+if(process.env.NODE_ENV !== 'production'){
+	console.log('Looks like we are in development mode!');
+}
 
 function component() {
 	// var element = document.createElement('div');
@@ -11,7 +15,7 @@ function component() {
 	var btn = document.createElement('button');
 
 	// element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
+	// element.classList.add('hello');
 
 	element.innerHTML = [
 		'Hello webpack!',
@@ -19,12 +23,12 @@ function component() {
 	].join('\n\n');
 
 	// 将图像添加到现有的div
-	var myIcon = new Image();
-	myIcon.src = Icon;
+	// var myIcon = new Image();
+	// myIcon.src = Icon;
 
-	element.appendChild(myIcon);
+	// element.appendChild(myIcon);
 
-	console.log(Data);
+	// console.log(Data);
 
 	btn.innerHTML = 'Click me and check the console!';
 	btn.onclick = printMe;
